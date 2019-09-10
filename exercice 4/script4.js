e75fd39dc99006138e243453c71ae1c327efe11e
Lozano Elian <Elian.lozano@outlook.fr>
2019-09-10 13:08:55 +0200

    exo 4 finish

--- exercice 4/script4.js
+++ exercice 4/script4.js
@@ -5,5 +5,7 @@ var cityInput = document.getElementById("ville");
 var btnElement = document.getElementsByTagName("button")[0];
 
 btnElement.addEventListener("click", function(){
-    .value("")
+    nameInput.value = "";
+    firstNameInput.value = "";
+    cityInput.value = "";
 });
