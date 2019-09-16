9efc43a6e64c43444dad351b50c8db7337a34f12
Lozano Elian <Elian.lozano@outlook.fr>
2019-09-16 10:59:27 +0200

    base 2 finish

---
+++ exercice 5/script5.js
@@ -0,0 +1,11 @@
+
+
+for (let i = 1; i < 6; i++) {
+    
+    document.getElementById("image"+i).addEventListener("mouseover", function(){
+        document.getElementById("image"+i).src = "images/image"+i+"_2.jpg"
+    })
+    document.getElementById("image"+i).addEventListener("mouseleave", function(){
+        document.getElementById("image"+i).src = "images/image"+i+".jpg"
+    })
+}
